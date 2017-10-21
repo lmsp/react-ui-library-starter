d46a5555de8f464aa0b08f0445b44ec0939cd7eb
lmsp <lmsp@opoloxoloqo.org>
2017-10-21 06:56:39 +0200

    chore(app): Añadir proceso de documentación

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 /* @flow */
+/**
+ * Un objeto Index
+ */
 export default class Index {
   constructor () {
     this._name = 'MyIndex'
   }
+  /**
+   * Devuelve un nombre
+   * @returns {String} el nombre actual
+   */
   get name () {
     return this._name
   }
